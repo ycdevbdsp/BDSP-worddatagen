a78a827bfcf77c26699301c8a2a4a82c41ac9a44
jaredcox000 <jaredcoxgamedev@gmail.com>
2022-01-12 19:45:47 -0600

    fixed strWidth calcs
    I was calculating strWidth incorrectly due to a bug with spaces not being considered correctly.

--- worddatagenerator.py
+++ worddatagenerator.py
@@ -13,11 +13,7 @@ def main():
     
     while building:
         message = input("Input your message: ")
-        
-        if message[0] == '#':
-            message = message.replace("#", "", 1)
-            eventID = 7
-        
+
         message = message.replace('\\n', '\n')
         messageArray = re.split('( |\n)', message)
         #messageArray = message.split(" ") #message split by spaces
@@ -27,67 +23,69 @@ def main():
         newMessage = [] #each individual new line, to be inserted into txt
         newMessage.append("")
         newMessageCounter = 0 #counter used for accessing current line
-        
-        words = len(messageArray)
-        index = 0
-        print(words)
+
+        numWords = len(messageArray)
+        wordCount = 1
         for word in messageArray:
-            print(word)
-            if word == ' ':
-                index += 1
-                continue
-            elif word != '\n':
+            if word != '\n':
                 wordLength = calculator.calculate(word)
 
-            if subTotal[subTotalCounter] + wordLength > 600 or index == words or word == '\n':
-                newMessage[0] = newMessage[0].replace("'", "’")
+                if subTotal[subTotalCounter] + wordLength > 600:
+                    newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+                    newMessageCounter += 1
+                    newMessage.append("")
+                    subTotalCounter += 1
+                    subTotal.append(0.0)
 
-                wordData = {
-                    "patternID": 7,
-                    "eventID": eventID,
-                    "tagIndex": -1,
-                    "tagValue": 0.0,
-                    "str": newMessage[0],
-                    "strWidth": subTotal[0]
-                }
-                
-                if eventID == 1:
-                    eventID = 3
-                elif eventID == 3:
-                    eventID = 1
-                    
-                output['wordDataArray'].append(wordData)
+                newMessage[newMessageCounter] += word
 
-                subTotal.clear()
-                subTotal.append(0.0)
-                newMessage.clear()
+                subTotal[subTotalCounter] += wordLength
+            else:
+                newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+                newMessageCounter += 1
                 newMessage.append("")
+                subTotalCounter += 1
+                subTotal.append(0.0)
 
-            if word == '\n':
-                index += 1
-                continue
+            wordCount += 1
 
-            newMessage[newMessageCounter] += word + " "
-            subTotal[subTotalCounter] += wordLength
+        subTotal.clear()
+        newMessageIterator = copy.deepcopy(newMessage)
+        newMessage.clear()
+
+        index = 1
+        patternID = 7
+        eventID = 1
+        count = len(newMessageIterator)
+
+        for line in newMessageIterator:
+            line.strip()
+
+            if index == count:
+                patternID = 0
+                eventID = 7
+
+            wordData = {
+                "patternID": patternID,
+                "eventID": eventID,
+                "tagIndex": -1,
+                "tagValue": 0.0,
+                "str": line,
+                "strWidth": calculator.calculate(line)
+            }
+                
+            if eventID == 1:
+                eventID = 3
+            elif eventID == 3:
+                eventID = 1
+                    
+            output['wordDataArray'].append(wordData)
             index += 1
 
-        if index == words:
-            if len(newMessage[0]) > 0:
-                newMessage[0] = newMessage[0].replace("'", "’")
-                wordData = {
-                    "patternID": 7,
-                    "eventID": 7,
-                    "tagIndex": -1,
-                    "tagValue": 0.0,
-                    "str": newMessage[0],
-                    "strWidth": subTotal[0]
-                }
-                output['wordDataArray'].append(wordData)
-
-            output['wordDataArray'][len(output['wordDataArray']) - 1]['eventID'] = 7
-            with open('wordData.json', 'w+', encoding="utf-8") as outfile:
-                json.dump(output, outfile)
-            quit()
+        output['wordDataArray'][len(output['wordDataArray']) - 1]['eventID'] = 7
+        with open('wordData.json', 'w+', encoding="utf-8") as outfile:
+            json.dump(output, outfile)
+        quit()
 
 
 if (__name__ == "__main__"):
