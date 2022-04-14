679329aa69a46cded4f4c74a22ece9d4308bdfec
jaredcox000 <jaredcoxgamedev@gmail.com>
2022-04-14 16:42:02 -0500

    added DSPRE format specifiers
    \f and \r can be used as if it were DSPRE. Also added \w to force wait times (static 2 seconds right now) in dialog.

--- worddatagenerator.py
+++ worddatagenerator.py
@@ -1,4 +1,5 @@
 import re
+from tkinter import N
 import rapidjson
 from generateHeader import addHeader
 import stringLengthCalculator as calculator
@@ -39,6 +40,8 @@ def convert2BDSP(message, labelIndex, arrayIndex, printOutput = True, label = No
     newMessage = [] #each individual new line, to be inserted into txt
     newMessage.append("")
     newMessageCounter = 0 #counter used for accessing current line
+    eventIDList = []
+    eventIDList.append(0)
 
     numWords = len(messageArray)
     wordCount = 1
@@ -46,23 +49,107 @@ def convert2BDSP(message, labelIndex, arrayIndex, printOutput = True, label = No
         if printOutput:
             print(word)
 
-        if word != '\n' and "<name>" not in word:
+        words = []
+
+        if word != '\n' and '\\r' not in word and '\\f' not in word and '\\w' not in word and "<name>" not in word:
             wordLength = calculator.calculate(word)
 
             if subTotal[subTotalCounter] + wordLength > 660:
+                newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+                eventIDList[newMessageCounter] = 4
+                newMessageCounter += 1
+                eventIDList.append(0)
+                newMessage.append("")
+                subTotalCounter += 1
+                subTotal.append(0.0)
+
+                # #we might have a format specifier between two words all lumped together, throwing off the calculated
+                # #total, so check for that here. But even if it does go over the limit, each specifier results in the
+                # #end of a line regardless.
+
+                # if '\\r' in word:
+                #     words = re.split('\\\\r', word)
+                #     eventIDList[newMessageCounter] = 3
+                #     eventIDList.append(0)
+
+                # elif '\\f' in word:
+                #     words = re.split('\\\\f', word)
+                #     eventIDList[newMessageCounter] = 4
+                #     eventIDList.append(0)
+
+                # elif '\\w' in word:
+                #     words = re.split('\\\\w', word)
+                #     eventIDList[newMessageCounter] = 2
+                #     eventIDList.append(0)
+
+                # if len(words) > 0:
+                #     word = words[0]
+                #     wordLength = calculator.calculate(words)
+
+                #     if subTotal[subTotalCounter] + wordLength > 660:
+                #         newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+                #         newMessageCounter += 1
+                #         newMessage.append("")
+                #         subTotalCounter += 1
+                #         subTotal.append(0.0)
+
+                #     messageArray.insert(wordCount, words[1])
+
+            newMessage[newMessageCounter] += word
+
+            subTotal[subTotalCounter] += wordLength
+        else:
+            event = []
+            if word == '\n':
+                eventIDList[newMessageCounter] = 1
+
+            if '\\r' in word:
+                event = re.split('\\\\r', word)
+                eventIDList[newMessageCounter] = 3
+            
+            if '\\w' in word:
+                event = re.split('\\\\w', word)
+                eventIDList[newMessageCounter] = 2
+
+            if '\\f' in word:
+                event = re.split('\\\\f', word)
+                eventIDList[newMessageCounter] = 4
+
+            if len(event) > 0:
+                #the first side of the split gets added to the current newMessageCounter just as if it were read off
+                #the line. If it wraps to a new line, that's fine.
+
+                wordLength = calculator.calculate(event[0])
+
+                if subTotal[subTotalCounter] + wordLength > 660:
+                    newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+                    newMessageCounter += 1
+                    newMessage.append("")
+                    subTotalCounter += 1
+                    subTotal.append(0.0)
+
+                newMessage[newMessageCounter] += event[0]
+                subTotal[subTotalCounter] += wordLength
+
+                #The second side of the split always starts on an incremented newMessageCounter, so we need to increment
+                #both the newMessageCounter and subTotalCounter here.
+
                 newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
                 newMessageCounter += 1
                 newMessage.append("")
                 subTotalCounter += 1
                 subTotal.append(0.0)
 
-            newMessage[newMessageCounter] += word
+                newMessage[newMessageCounter] += event[1]
+                subTotal[subTotalCounter] += wordLength
 
-            subTotal[subTotalCounter] += wordLength
-        else:
-            newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
-            newMessageCounter += 1
-            newMessage.append("")
+            eventIDList.append(0)
+            # newMessage[newMessageCounter] = newMessage[newMessageCounter].replace("'", "’")
+            # newMessageCounter += 1
+
+            if len(event) == 0:
+                newMessage.append("")
+
             subTotalCounter += 1
             subTotal.append(0.0)
 
@@ -83,6 +170,7 @@ def convert2BDSP(message, labelIndex, arrayIndex, printOutput = True, label = No
                     raise Exception ("Nothing can precede <name> unless it is the beginning of a message.")
 
                 print(insertString)
+                eventIDList[newMessageCounter] = 5
                 newMessage[newMessageCounter] += insertString[1]
                 newMessageCounter += 1
                 newMessage.append("")
@@ -100,20 +188,43 @@ def convert2BDSP(message, labelIndex, arrayIndex, printOutput = True, label = No
     index = 1
     patternID = 7
     eventID = 1
+    eventIDListIndex = 0
     count = len(newMessageIterator)
+    print(len(eventIDList))
 
+    #The first entry in worddata always has eventID of 1, so force that here.
+    #It's easier to do it this way than to provide logic above to only use 1 in the first index.
+
+    eventIDList[0] = 1
+    precedingFinal = False
     for line in newMessageIterator:
         line.strip()
+
+        eventID = eventIDList[eventIDListIndex]
+        tagValue = 0.0
+
+        if index + 1 == count:
+            precedingFinal = True
 
         if index == count:
             patternID = 0
             eventID = 7
 
+        if eventID == 2:
+            tagValue = 1.9999999999
+
+        # There's something weird with how worddatas proceed when it comes to eventIDs 3 and 4 being mixed together
+        # and capped with an eventID 7. I've found that effectively an eventID of 4 should not precede an eventID of 7.
+        # If this happens, replace eventID 4 with 1.
+
+        if precedingFinal == True and eventID == 4:
+            eventID = 1
+
         wordData = {
             "patternID": patternID,
             "eventID": eventID,
             "tagIndex": -1,
-            "tagValue": 0.0,
+            "tagValue": tagValue,
             "str": line,
             "strWidth": calculator.calculate(line)
         }
@@ -125,11 +236,7 @@ def convert2BDSP(message, labelIndex, arrayIndex, printOutput = True, label = No
             wordData["str"] = ""
             wordData["strWidth"] = -1.0
 
-        if eventID == 1:
-            eventID = 3
-        elif eventID == 3:
-            eventID = 1
-
+        eventIDListIndex += 1
         output['wordDataArray'].append(wordData)
         index += 1
 
